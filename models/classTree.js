16a7a6d6035e4c22500c88a6853908a41587192b
hs.o <ohs2033@gmail.com>
2016-05-25 15:42:46 +0900

    add some comment

--- models/classTree.js
+++ models/classTree.js
@@ -6,7 +6,7 @@ const ClassTreeSchema = new Schema({
 		type: Schema.ObjectId,
 		ref: 'ClassTree'
 	},
-	//자기 자신의 id들을 담고 있는 배열.
+	//자기 자신의 id들을 담고 있는 배열입니다. ClassTree는 트리의 노드나 마찬가지입니다.
 	children:[{type:ObjectId, ref:'ClassTree'}]
 })
 
