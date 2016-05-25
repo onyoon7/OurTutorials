60d70137ebb8209bc417b24c6bd873106f928c16
prodia4 <prodia4@gmail.com>
2016-05-25 20:47:16 +0900

    add karma configuration(with karma-webpack)

---
+++ karma.conf.js
@@ -0,0 +1,72 @@
+// Karma configuration
+// Generated on Fri May 20 2016 18:40:01 GMT+0900 (KST)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: './',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['mocha','chai','sinon'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+        'test/test_bundle.js',
+        //'test/*_spec.js',
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'test/test_bundle.js': ['webpack'],
+      //'test/*_spec.js': ['webpack'],
+    },
+
+    webpack: {
+      module: {
+        loaders: [{
+          test: /\.jsx?$/,
+          exclude: /(bower_components|node_modules)/,
+          loader:'babel'
+        }]
+      }
+    },
+    webpackMiddleware: {
+      noInfo: false,
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['spec'],
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    //autoWatch: false,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    //singleRun: true,
+  });
+};
