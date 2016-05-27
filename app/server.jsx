568e766fb0048f928475445777fa85536ddd34db
prodia4 <prodia4@gmail.com>
2016-05-27 21:17:05 +0900

    (server-setting) add comments(server.jsx), modify title(Meta.jsx) and eslint(package.json)

--- app/server.jsx
+++ app/server.jsx
@@ -1,10 +1,21 @@
+// promise 기반 http 클라이언트
 import axios from 'axios';
 import React from 'react';
+
+// 서버 쪽에서 component를 render할 때 사용. ReactElement를 HTML로 만들어준다.
 import { renderToString } from 'react-dom/server';
+
+// CreateMemoryHistory - 인메모리 history(object that contains URLs visited by the user) 객체를 생성한다.
+
+// match({routes, location, [history], ...options}, cb) - 서버 쪽에 들어온 location(예를 들면 req.url)이 주어진 routes에 매칭되는지 확인하고 각각의 경우에 따라 callback 함수를 호출한다. callback 함수는 error, redirectLocation, renderProps 세 가지 인자를 가지는데, 셋 다 undefined일 경우는 location에 해당되는 route가 없을 경우다.
+
+// RouterContext - renders the component tree for a given router state. Its used by <Router> but also useful for server rendering and integrating in brownfield development.
 import { createMemoryHistory, match, RouterContext } from 'react-router';
 import { Provider } from 'react-redux';
 import createRoutes from 'routes';
 import configureStore from 'store/configureStore';
+
+// 컴포넌트의 static need를 보고, dispatch(need(params))가 모두 끝날 때까지 기다린다.
 import preRenderMiddleware from 'middlewares/preRenderMiddleware';
 import header from 'components/Meta';
 
@@ -24,6 +35,7 @@ axios.defaults.baseURL = `http://${clientConfig.host}:${clientConfig.port}`;
 export default function render(req, res) {
   const authenticated = req.isAuthenticated();
   const history = createMemoryHistory();
+  // redux-thunk, redux-logger등을 미들웨어로 가지는 store 생성
   const store = configureStore({
     user: {
       authenticated,
@@ -32,6 +44,7 @@ export default function render(req, res) {
       isLogin: true
     }
   }, history);
+  // store를 받아서 '/', 'login', 'dashboard', 'about' 경로에 대한 component를 정의한 route를 반환한다.
   const routes = createRoutes(store);
 
   /*
