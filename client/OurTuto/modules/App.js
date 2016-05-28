4b0d4e2ada203b7064a09440587edac33c678ffb
gkddy <zalhanilll@gmail.com>
2016-05-28 13:19:50 +0900

    update client

---
+++ client/OurTuto/modules/App.js
@@ -0,0 +1,132 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+export default React.createClass({
+  render() {
+    return (
+    <div>
+{/*
+  <div className="navbar-wrapper">
+    <div className="container">
+
+      <div className="navbar navbar-default navbar-fixed-top" role="navigation" id="top-nav">
+        <div className="container">
+          <div className="navbar-header">
+         <ul>
+          <li><Link to="/Signup">회원가입</Link></li>
+          <li><Link to="/Login">로그인</Link></li>
+          <li><Link to="/Course">코스</Link></li>
+          <li><Link to="/">튜토리얼</Link></li>
+          <li id ="ourlogo"><Link to="/"><img src="images/logo.png"/></Link></li>
+        </ul>
+        {this.props.children}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+*/}
+{/*figure 요소는 사진 도표 삽화 오디오 비디오 등을 담는 컨테이너 역할을 하는 태그*/}
+        <div id="works"  className=" clearfix grid">
+          <figure className="effect-oscar  wowload fadeInUp">
+              <img src="images/portfolio/1.png" alt="img01"/>
+              <figcaption>
+                <h3>python</h3>
+                <Link to="/Choose"></Link>
+              </figcaption>
+          </figure>
+
+          <figure className="effect-oscar  wowload fadeInUp">
+              <img src="images/portfolio/2.png" alt="img01"/>
+              <figcaption>
+                <h3>Javascrip</h3>
+                <Link to="/Choose"></Link>
+              </figcaption>
+          </figure>
+
+          <figure className="effect-oscar  wowload fadeInUp">
+              <img src="images/portfolio/3.png" alt="img01"/>
+              <figcaption>
+                  <h3>swift</h3>
+                  <Link to="/Choose"></Link>
+              </figcaption>
+          </figure>
+
+          <figure className="effect-oscar  wowload fadeInUp">
+              <img src="images/portfolio/4.png" alt="img01"/>
+              <figcaption>
+                  <h3>html</h3>
+                  <Link to="/Choose"></Link>
+              </figcaption>
+          </figure>
+
+          <figure className="effect-oscar  wowload fadeInUp">
+              <img src="images/portfolio/5.png" alt="img01"/>
+              <figcaption>
+                  <h3>java</h3>
+                  <Link to="/Choose"></Link>
+              </figcaption>
+          </figure>
+
+          <figure className="effect-oscar  wowload fadeInUp">
+              <img src="images/portfolio/6.png" alt="img01"/>
+              <figcaption>
+                  <h3>go</h3>
+                  <Link to="/Choose"></Link>
+              </figcaption>
+          </figure>
+
+          <figure className="effect-oscar  wowload fadeInUp">
+              <img src="images/portfolio/5.png" alt="img01"/>
+              <figcaption>
+                  <h3>html</h3>
+                  <Link to="/Choose"></Link>
+              </figcaption>
+          </figure>
+
+          <figure className="effect-oscar  wowload fadeInUp">
+              <img src="images/portfolio/4.png" alt="img01"/>
+              <figcaption>
+                  <h3>java</h3>
+                  <Link to="/Choose"></Link>
+              </figcaption>
+          </figure>
+
+          <figure className="effect-oscar  wowload fadeInUp">
+              <img src="images/portfolio/5.png" alt="img01"/>
+              <figcaption>
+                  <h3>go</h3>
+                  <Link to="/Choose"></Link>
+              </figcaption>
+          </figure>
+
+          <figure className="effect-oscar  wowload fadeInUp">
+              <img src="images/portfolio/4.png" alt="img01"/>
+              <figcaption>
+                  <h3>html</h3>
+                  <Link to="/Choose"></Link>
+              </figcaption>
+          </figure>
+
+          <figure className="effect-oscar  wowload fadeInUp">
+              <img src="images/portfolio/5.png" alt="img01"/>
+              <figcaption>
+                  <h3>java</h3>
+                  <Link to="/Choose"></Link>
+              </figcaption>
+          </figure>
+
+          <figure className="effect-oscar  wowload fadeInUp">
+              <img src="images/portfolio/6.png" alt="img01"/>
+              <figcaption>
+                  <h3>go</h3>
+                  <Link to="/Choose"></Link>
+              </figcaption>
+          </figure>
+
+
+        </div>
+      </div>
+    )
+  }
+})
---
+++ client/OurTuto/modules/ChooseChild.js
@@ -0,0 +1,104 @@
+
+import React from 'react'
+import { Link } from 'react-router'
+
+export default React.createClass({
+  render() {
+    return (
+    <div>
+
+        <div id="works"  className=" clearfix gridChild">
+
+          <figure className="effect-oscar  wowload fadeInUp">
+
+              <img src="images/link/12l.png" alt="img01"/>
+              <figcaption>
+                <a href="https://www.google.com/" target="_black">1등</a>
+              </figcaption>
+          </figure>
+
+          <figure className="effect-oscar  wowload fadeInUp">
+
+              <img src="images/link/11l.png" alt="img01"/>
+              <figcaption>
+                <a href="https://www.google.com/" target="_black">2등</a>
+              </figcaption>
+          </figure>
+
+          <figure className="effect-oscar  wowload fadeInUp">
+
+              <img src="images/link/10l.png" alt="img01"/>
+              <figcaption>
+                <a href="https://www.google.com/" target="_black">3등</a>
+              </figcaption>
+          </figure>
+
+          <figure className="effect-oscar  wowload fadeInUp">
+
+              <img src="images/link/9l.png" alt="img01"/>
+              <figcaption>
+                <a href="https://www.google.com/" target="_black">4등</a>
+              </figcaption>
+          </figure>
+
+          <figure className="effect-oscar  wowload fadeInUp">
+
+              <img src="images/link/8l.png" alt="img01"/>
+              <figcaption>
+                <a href="https://www.google.com/" target="_black">5등</a>
+              </figcaption>
+          </figure>
+
+          <figure className="effect-oscar  wowload fadeInUp">
+
+              <img src="images/link/7l.png" alt="img01"/>
+              <figcaption>
+                <a href="https://www.google.com/" target="_black">6등</a>
+              </figcaption>
+          </figure>
+
+          <figure className="effect-oscar  wowload fadeInUp">
+
+              <img src="images/link/6l.png" alt="img01"/>
+              <figcaption>
+                <a href="https://www.google.com/" target="_black">7등</a>
+              </figcaption>
+          </figure>
+
+          <figure className="effect-oscar  wowload fadeInUp">
+
+              <img src="images/link/5l.png" alt="img01"/>
+              <figcaption>
+                <a href="https://www.google.com/" target="_black">8등</a>
+              </figcaption>
+          </figure>
+
+          <figure className="effect-oscar  wowload fadeInUp">
+
+              <img src="images/link/4l.png" alt="img01"/>
+              <figcaption>
+                <a href="https://www.google.com/" target="_black">asdf</a>
+              </figcaption>
+          </figure>
+
+          <figure className="effect-oscar  wowload fadeInUp">
+
+              <img src="images/link/3l.png" alt="img01"/>
+              <figcaption>
+                <a href="https://www.google.com/" target="_black">asdf</a>
+              </figcaption>
+          </figure>
+
+          <figure className="effect-oscar  wowload fadeInUp">
+
+              <img src="images/link/1l.png" alt="img01"/>
+              <figcaption>
+                <a href="https://www.google.com/" target="_black">asdf</a>
+              </figcaption>
+          </figure>
+        </div>
+
+      </div>
+    )
+  }
+})
